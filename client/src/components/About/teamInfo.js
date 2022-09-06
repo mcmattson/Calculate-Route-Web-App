9f893d7a5c9580aa84ae7b9c5f0216cad935647b
maricodes <maria.lake.cs@gmail.com>
2022-09-06 11:33:12 -0600

    Adding Mari's bio

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -30,7 +30,7 @@ export const memberData = [
     },
     {
         name: "Team Member 4",
-        bio: "Bio Here",
+        bio: "I'm a junior at Colorado State University majoring in Computer Science and concentrating on Software Engineering. I am currently a UTA for CS165: Data Structures and Algorithms. I have a strong interest in Software Engineering in the aerospace industry and I plan to pursue that path after finishing my degree. My favorite achievement is that I'm a four time Dean's List student.",
         homeTown: "Hometown",
         imagePath: memberPic
     },
