b3be1e21718d41e7d50d438dde6f090f934c1ae0
CathWar <cathryn2731@yahoo.com>
2022-09-22 17:09:17 -0600

    Committed first few tests.

--- server/src/test/java/com/tco/misc/TestDistanceCalculator.java
+++ server/src/test/java/com/tco/misc/TestDistanceCalculator.java
@@ -27,6 +27,40 @@ public class TestDistanceCalculator {
     }
     
     //Testcases will go below: 
+    final Geo origin = new Geo(0.0, 0.0);
+    final Geo coordinatesBurma = new Geo(21.9162, 95.9560);
+
+    //Testing minimum Earth radius value.
+    final static long small = 1L;
+    final static long piSmall = Math.round(Math.PI * small);
+    final static long piSmallHalf = Math.round(Math.PI / 2.0 * small);
+
+    //Testing large values with significant digits to verify double/long.
+    final static long big = 1000000000000L;
+    final static long piBig = Math.round(Math.PI * big);
+    final static long piBigHalf = Math.round(Math.PI / 2.0 * big);
+
+    //Big identity tests.
+    @Test
+    @DisplayName("cath2731: The distance-to-self should be zero.")
+    public void testDistanceToSelf() {
+        assertEquals(0L, calculate(origin, origin, big));
+        assertEquals(0L, calculate(origin, origin, small));
+    }
+
+    @Test
+    @DisplayName("cath2731: The distance-to-self should be zero.")
+    public void testDistanceToSelfPi() {
+        assertEquals(0L, calculate(origin, origin, piBig));
+        assertEquals(0L, calculate(origin, origin, piSmall));
+    }
+
+    @Test
+    @DisplayName("cath2731: The distance-to-self should be zero.")
+    public void testDistanceToSelf() {
+        assertEquals(0L, calculate(origin, origin, piBigHalf));
+        assertEquals(0L, calculate(origin, origin, piSmallHalf));
+    }
 
 
 
