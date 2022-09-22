65600d4993ce1d2dc55f9bc02afb38ed971de5e4
maricodes <[maria.lake.cs@gmail.com]>
2022-09-22 16:43:31 -0600

    this commit consists of a singular semi colon. I should have ran my code before committing. I thought it worked. Dumb thought.

--- server/src/test/java/com/tco/misc/TestDistanceCalculator.java
+++ server/src/test/java/com/tco/misc/TestDistanceCalculator.java
@@ -6,7 +6,7 @@ import static java.lang.Math.toRadians;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
