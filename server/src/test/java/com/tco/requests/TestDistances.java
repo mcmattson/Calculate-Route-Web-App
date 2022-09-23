988fd78c2a0dad1042c34106e251f7223efcee3e
Matt <83243547+mcmattson@users.noreply.github.com>
2022-09-22 21:30:41 -0600

    Update TestDistances.java

--- server/src/test/java/com/tco/requests/TestDistances.java
+++ server/src/test/java/com/tco/requests/TestDistances.java
@@ -14,4 +14,13 @@ public class TestDistances {
         assertEquals(0, distances.size());
         assertEquals(0L, distances.total());
     }
+    
+    @Test
+    @DisplayName ("mmattson: distances with 1 place")
+    public void testOneDistances () {
+        Distances distances = new Distances();
+        distances.add(12345L);
+        assertEquals(1, distances.size());
+        assertEquals(12345L, distances.total());
+    }
 }
