54896f78ee6bab7d626acbda84d0ef2de87c1a76
Matt <83243547+mcmattson@users.noreply.github.com>
2022-09-22 19:55:20 -0600

    Add files via upload

---
+++ server/src/test/java/com/tco/requests/TestDistances.java
@@ -0,0 +1,16 @@
+package com.tco.requests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDistances {
+
+    @Test
+    @DisplayName ("mmattson: distances with no places")
+    public void testNoDistances () {
+        Distances distances = new Distances();
+        assertEquals(0, distances.size());
+        assertEquals(0L, distances.total());
+    }
+}
