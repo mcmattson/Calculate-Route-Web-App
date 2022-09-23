61e101a7317da35f0c7938984e2ce8d7264d3a42
Matt <83243547+mcmattson@users.noreply.github.com>
2022-09-22 20:57:56 -0600

    Add files via upload

--- server/src/test/java/com/tco/requests/TestDistances.java
+++ server/src/test/java/com/tco/requests/TestDistances.java
@@ -1,3 +1,4 @@
+//Credit Dave Matthews
 package com.tco.requests;
 
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,37 @@ public class TestDistances {
         Distances distances = new Distances();
         assertEquals(0, distances.size());
         assertEquals(0L, distances.total());
+    }
+
+    @Test
+    @DisplayName ("mmattson: distances with 1 place")
+    public void testOneoDistances () {
+        Distances distances = new Distances();
+        distances.add(12345L);
+        assertEquals(1, distances.size());
+        assertEquals(12345L, distances.total());
+    }
+
+    @Test
+    @DisplayName ("mmattson: two distances with no places")
+    public void testTwoDistances () {
+        Distances distances = new Distances();
+        distances.add(0L);
+        distances.add(0L);
+        assertEquals(2, distances.size());
+        assertEquals(0L, distances.total());
+    }
+
+    @Test
+    @DisplayName ("mmattson: distances with multiple places")
+    public void testDistances () {
+        Distances distances = new Distances();
+        distances.add(5L);
+        distances.add(40L);
+        distances.add(300L);
+        distances.add(2000L);
+        distances.add(10000L);
+        assertEquals(5, distances.size());
+        assertEquals(12345L, distances.total());
     }
 }
