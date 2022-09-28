5f266801b7f23743b729c315576bb5d646c19a41
Mikyla Bowen <mikyla.bowen@colostate.edu>
2022-09-27 19:18:04 -0600

    Basic distance code. Needs to be tested more, and a test needs to be fixed to allow it to run.

--- server/src/main/java/com/tco/misc/DistanceCalculator.java
+++ server/src/main/java/com/tco/misc/DistanceCalculator.java
@@ -7,7 +7,23 @@ public final class DistanceCalculator {
     private DistanceCalculator() {}
 
     public static Long calculate(GeographicCoordinate from, GeographicCoordinate to, double earthRadius){
-        return 0L;
+        Double delta_lambda = to.lonRadians() - from.lonRadians();
+
+        Double first_group = Math.cos(to.latRadians()) * Math.sin(delta_lambda);
+        Double second_group = first_group * first_group;
+
+        Double cosSinLat = Math.cos(from.latRadians()) * Math.sin(to.latRadians());
+        Double sinCosCos = Math.sin(from.latRadians()) * Math.cos(to.latRadians()) * Math.cos(delta_lambda);
+        Double third_group = cosSinLat-sinCosCos;      
+        Double fourth_group = third_group *third_group;
+
+        Double numerator = Math.sqrt(second_group + fourth_group);
+        Double sinSin = Math.sin(from.latRadians()) * Math.sin(to.latRadians());
+        Double cosCosCos = Math.cos(from.latRadians()) * Math.cos(to.latRadians()) * Math.cos(delta_lambda);
+        Double demoninator = sinSin + cosCosCos;
+
+        Double delta_o = Math.atan2(numerator,demoninator);
+        return Math.round(delta_o * earthRadius); 
     }
 
 }
