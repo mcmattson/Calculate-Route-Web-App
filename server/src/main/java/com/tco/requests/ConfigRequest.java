38cde36c60dd4556fe19c219d2c424b2b439ed5e
Mikyla Bowen <mikyla.bowen@colostate.edu>
2022-09-28 19:19:55 -0600

    Add distances as a feature.

--- server/src/main/java/com/tco/requests/ConfigRequest.java
+++ server/src/main/java/com/tco/requests/ConfigRequest.java
@@ -18,6 +18,7 @@ public class ConfigRequest extends Request {
         serverName = "t16 Data Avengers";
         features = new ArrayList<>();
         features.add("config");
+        features.add("distances");
         log.trace("buildResponse -> {}", this);
     }
 
