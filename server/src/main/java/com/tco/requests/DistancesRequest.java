db083f08064db551a4f9cf9997a70cba88810a8f
maricodes <[maria.lake.cs@gmail.com]>
2022-10-03 12:02:55 -0600

    Brain is slowly deteriorating. SCRUM fixes.

--- server/src/main/java/com/tco/requests/DistancesRequest.java
+++ server/src/main/java/com/tco/requests/DistancesRequest.java
@@ -31,7 +31,7 @@ public class DistancesRequest extends Request {
         for(int i = 0; i < places.size(); i++ ){
             Place placeFrom = places.get(i);
             Place placeTo = places.get((i + 1) % places.size());
-            Long toAdd = DistanceCalculator.calculate(one, two, 6571.0);
+            Long toAdd = DistanceCalculator.calculate(placesFrom, placeTo, 6571.0);
             distances.add(toAdd);
         } 
         return distances;
