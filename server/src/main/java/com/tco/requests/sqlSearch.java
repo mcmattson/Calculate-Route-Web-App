aad98b8590e0d4cce48c2c7cbcbb50be1021713d
Mikyla Bowen <mikyla.bowen@colostate.edu>
2022-10-17 13:42:50 -0600

    Remove the remaining external functions.

--- server/src/main/java/com/tco/requests/sqlSearch.java
+++ server/src/main/java/com/tco/requests/sqlSearch.java
@@ -10,6 +10,7 @@ import java.lang.Exception;
 import com.tco.requests.Location;
 import com.tco.requests.Locations;
 import com.tco.requests.Report;
+import com.tco.requests.Credential;
 
 
 
@@ -18,8 +19,13 @@ public class sqlSearch {
 	private final static String TABLE = "world";
 	private final static String COLUMNS = "id,name,municipality,iso_region,iso_country,continent,latitude,longitude,altitude";
 
-	//static class Location extends HashMap<String,String> {}
-	//static class Locations extends ArrayList<Location> {}
+	public static String getCOLUMNS(){
+		return COLUMNS;
+	}
+
+	public static String getTABLE(){
+		return TABLE;
+	}
 
 	public static void main(String[] args) {
 		try {
@@ -34,92 +40,5 @@ public class sqlSearch {
 			System.err.println("Exception: " + e.getMessage());
 		}
 	}
-
-	static class Database {
-		static Integer found(String match) throws Exception {
-			String sql = Select.found(match);
-			try (
-				// connect to the database and query
-				Connection conn = DriverManager.getConnection(Credential.url(), Credential.USER, Credential.PASSWORD);
-				Statement query = conn.createStatement();
-				ResultSet results = query.executeQuery(sql)
-			) {
-				return count(results);
-			} catch (Exception e) {
-				throw e;
-			}
-		}
-
-		private static Integer count(ResultSet results) throws Exception {
-			if (results.next()) {
-				return results.getInt("count");
-			}
-			throw new Exception("No count results in found query.");
-		}
-
-
-		static Locations Locations(String match, Integer limit) throws Exception {
-			String sql      = Select.match(match, limit);
-			String url      = Credential.url();
-			String user     = Credential.USER;
-			String password = Credential.PASSWORD;
-			try (
-				// connect to the database and query
-				Connection conn    = DriverManager.getConnection(url, user, password);
-				Statement  query   = conn.createStatement();
-				ResultSet  results = query.executeQuery(sql)
-			) {
-				return convertQueryResultsToLocations(results, COLUMNS);
-			} catch (Exception e) {
-				throw e;
-			}
-		}
-
-
-		private static Locations convertQueryResultsToLocations(ResultSet results, String columns) throws Exception {
-			int count = 0;
-			String[] cols = columns.split(",");
-			Locations Locations = new Locations();
-			while (results.next()) {
-				Location Location = new Location();
-				for (String col: cols) {
-					Location.put(col, results.getString(col));
-				}
-				Location.put("index", String.format("%d",++count));
-				Locations.add(Location);
-			}
-			return Locations;
-		}
-	}
-	static class Credential {
-		// shared user with read-only access
-		final static String USER = "cs314-db";
-		final static String PASSWORD = "eiK5liet1uej";
-		// connection information when using port forwarding from localhost
-		final static String URL = "jdbc:mariadb://faure.cs.colostate.edu/cs314";
-		static String url() {
-			return URL;
-		}
-	}
-
-	static class Select {
-		static String match(String match, int limit) {
-			return statement(match, "DISTINCT " + COLUMNS, "LIMIT " + limit);
-		}
-
-		static String found(String match) {
-			return statement(match, "COUNT(*) AS count ", "");
-		}
-
-		static String statement(String match, String data, String limit) {
-			return "SELECT "
-				+ data
-				+ " FROM " + TABLE
-				+ " WHERE name LIKE \"%" + match + "%\" "
-				+ limit
-				+ " ;";
-		}
-	}
-    
 
 }
