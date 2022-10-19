4d177c9c347446b297527682d65de17fd4f065ee
maricodes <[maria.lake.cs@gmail.com]>
2022-10-19 14:22:52 -0600

    Added TestDatabase.java and tweeked Database.java

--- server/src/main/java/com/tco/requests/Database.java
+++ server/src/main/java/com/tco/requests/Database.java
@@ -1,30 +1,34 @@
 package com.tco.requests;
-import java.util.HashMap;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.lang.Exception;
 
 import com.tco.requests.Location;
 import com.tco.requests.Locations;
-import java.sql.ResultSet;
+
 
 
 public class Database {
     
-
-
-private static Locations convertQueryResultsToLocations(ResultSet results, String columns) throws Exception {
-    int count = 0;
-    String[] cols = columns.split(",");
-    Locations Locations = new Locations();
-    while (results.next()) {
-        Location Location = new Location(); 
-        for (String col : cols){
-            Location.put(col, results.getString(col));
-        }
-        Location.put("index", String.format("%d", ++count));
+    public static Locations convertQueryResultsToLocations(ResultSet results, String columns) throws Exception {
+        int count = 0;
+        String[] cols = columns.split(",");
+        Locations Locations = new Locations();
+        while (results.next()) {
+        Location Location = new Location();  
+            for (String col : cols){
+                Location.put(col, results.getString(col));
+            }
+        Location.put("index", String.format("%d", ++count));    
         Locations.add(Location);  
+        }
+        return Locations;
     }
-    return Locations;
-}
 
 
 
