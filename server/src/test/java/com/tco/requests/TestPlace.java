722a576935dd4da6e59c0ad11cc6952bb429e30b
maricodes <maria.lake.cs@gmail.com>
2022-10-27 14:18:42 -0600

    Creating the class

---
+++ server/src/test/java/com/tco/requests/TestPlace.java
@@ -0,0 +1,14 @@
+package com.tco.requests;
+
+import static com.tco.requests.Place;
+import static java.lang.Math.toRadians;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestPlace {
+    
+}
