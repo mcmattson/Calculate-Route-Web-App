7a3086d9af4d8a3ccc7aa2f9c6ea1d0ff9cd05c4
maricodes <maria.lake.cs@gmail.com>
2022-10-27 11:30:15 -0600

    Creating testGeographicCoordinate class & imports

---
+++ server/src/test/java/com/tco/misc/TestGeographicCoordinate.java
@@ -0,0 +1,11 @@
+package com.tco.misc;
+
+import static java.lang.Math.toRadians;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class TestGeographicCoordinate {
+    
+
+}
