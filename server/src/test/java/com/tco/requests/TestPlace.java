101f5403674687bd9e5e165b01b26ae265a6e4d9
maricodes <maria.lake.cs@gmail.com>
2022-10-27 14:22:17 -0600

    Making test Places

--- server/src/test/java/com/tco/requests/TestPlace.java
+++ server/src/test/java/com/tco/requests/TestPlace.java
@@ -1,6 +1,6 @@
 package com.tco.requests;
 
-import static com.tco.requests.Place;
+import com.tco.requests.Place;
 import static java.lang.Math.toRadians;
 
 import org.junit.jupiter.api.Test;
@@ -10,5 +10,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestPlace {
-    
+
+    Place origin = new Place("0.0", "0.0");
+    Place coordinatesBurma = new Place("21.9162", "95.9560");
+    Place coordinatesFoCo = new Place("40.5853" , "-105.0844");
+    Place coordinatesAntarctic = new Place("-82.8628", "135.0000");
+
 }
