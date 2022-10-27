854b77991cfe148f8a433a3cc769550f633b8e2b
maricodes <maria.lake.cs@gmail.com>
2022-10-27 11:44:40 -0600

    Adding test geo

--- server/src/test/java/com/tco/misc/TestGeographicCoordinate.java
+++ server/src/test/java/com/tco/misc/TestGeographicCoordinate.java
@@ -6,6 +6,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class TestGeographicCoordinate {
-    
+    static class Geo implements GeographicCoordinate{
+        Double degreesLatitude; 
+        Double degreesLongitude;
+
+        public Geo(Double lat, Double lon){
+            this.degreesLatitude = lat;
+            this.degreesLongitude = lon;
+        }
+
+        public Double latRadians(){ return toRadians(degreesLatitude); }
+        public Double lonRadians(){ return toRadians(degreesLongitude); }
+
+    }
+
+    Geo geoCoord = new Geo(45.000,90.000);
+    Double geoLatToRadians = toRadians(45.000);
+    Double geoLonToRadians = toRadians(90.000);
 
 }
