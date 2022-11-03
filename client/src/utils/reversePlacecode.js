929220d31b460a5ee0b464a465ecb6b5081b51c3
Matt <matthewcmattson@gmail.com>
2022-11-02 18:08:11 -0600

    Add place now adds name search to map

--- client/src/utils/reversePlacecode.js
+++ client/src/utils/reversePlacecode.js
@@ -3,21 +3,21 @@ import { Place } from "../models/place.model";
 const GEOCODE_URL = "https://nominatim.openstreetmap.org/search?format=jsonv2";
 
 async function reversePlacecode(place) {
-    // Here the coordinates are in latLng format - example: {lat: 0, lng: 0}
+    // Here the place search are in string format - example: {Dave}
     const API_URL = GEOCODE_URL + `&q=${place}&countrycodes=us`;
-    console.log('reversePlacecode API_URL: ' + API_URL);
+    //console.log('reversePlacecode API_URL: ' + API_URL);
     const data = await fetch(API_URL);
     //console.log('reversePlacecode data: ' + data);
     const dataJSON = await data.json();
     //console.log('reversePlacecode dataJSON: ' + dataJSON);
    
-    for (var i = 0; i < dataJSON.length; i++) {
-        console.log("reversePlacecode [" + i + "]: " + dataJSON[i].display_name); 
-    }
+    /* for (var i = 0; i < dataJSON.length; i++) {
+        console.log("reversePlacecode [" + i + "]: " + dataJSON[i].display_name + " " + dataJSON[i].lat + " " + dataJSON[i].lon); 
+    } */
 
     const formattedPlace = place;
     if (dataJSON[0].display_name) {
-        const newPlace = new Place({ name: dataJSON[0].display_name});
+        const newPlace = new Place({ ...formattedPlace, name: dataJSON[0].display_name, lat: dataJSON[0].lat, longitude: dataJSON[0].lon, });
         return newPlace;
     } else {
         const unknownPlace = new Place({ name: 'Unkown', ...formattedPlace });
