f80921a49da041ff3cdc609d3b8554a3d3bc9f8b
maricodes <maria.lake.cs@gmail.com>
2022-11-03 11:53:30 -0600

    fixing useDistances + reworking Itenerary

--- client/src/components/Trip/Itinerary/Itinerary.js
+++ client/src/components/Trip/Itinerary/Itinerary.js
@@ -9,169 +9,196 @@ import { getOriginalServerUrl, sendAPIRequest } from '../../../utils/restfulAPI'
 
 
 export default function Itinerary(props) {
-	const [distanceSettings, processServerDistanceSuccess] = useDistances(props.places);
-	return (
-		<Table responsive>
-			<TripHeader
-				tripName={props.tripName}
-				distanceSettings= {distanceSettings}
-			/>
-			<PlaceList
-				places={props.places}
-				placeActions={props.placeActions}
-				selectedIndex={props.selectedIndex}
-				distanceSettings= {distanceSettings}
+    const distanceSettings = useDistances(props.places, 6371.0, getOriginalServerUrl());
+    return (
+        <Table responsive>
+            <TripHeader
+                tripName={props.tripName}
+                distanceSettings= {distanceSettings}
+            />
+            <PlaceList
+                places={props.places}
+                placeActions={props.placeActions}
+                selectedIndex={props.selectedIndex}
+                distanceSettings= {distanceSettings}
 
-			/>
-		</Table>
-	);
+            />
+        </Table>
+    );
 }
 
 function TripHeader(props) {
-	return (
-		<thead>
-			<tr>
-				 <TotalTripDistance distanceSettings = {props.distanceSettings} /> 
-			</tr>
-			<tr>
-				<th
-					className='trip-header-title'
-					data-testid='trip-header-title'
-				>
-					{props.tripName}
-				</th>
-				<th> Leg Distance (Kilometers) </th>
-				<th> Cumulative Distance (Kilometers)</th>
-			</tr>
-		</thead>
-	);
+    return (
+        <thead>
+            <tr>
+                 <TotalTripDistance distanceSettings = {props.distanceSettings} /> 
+            </tr>
+            <tr>
+                <th
+                    className='trip-header-title'
+                    data-testid='trip-header-title'
+                >
+                    {props.tripName}
+                </th>
+                <th> Leg Distance (Kilometers) </th>
+                <th> Cumulative Distance (Kilometers)</th>
+            </tr>
+        </thead>
+    );
 }
 
 function TotalTripDistance(props){
-	let distances = [0]; 
-	if (props.distanceSettings.serverDistance){ 
-	 	distances = props.distanceSettings.serverDistance.distances;
-	}
-	let total = 0 
-	for (var i = 0; i < distances.length; i++){ total+= distances[i];}
+    let distances = [0]; 
+    if (props.distanceSettings.distances){ 
+        distances = props.distanceSettings.distances;
+    }
+    let total = distances.total;
 
 
-	return (
-		<th	 data-testid = {'header-total-dist'} > Total Trip Distance (Kilometers): {total}</th>
-	)
+    return (
+        <th  data-testid = {'header-total-dist'} > Total Trip Distance (Kilometers): {total}</th>
+    )
 
 }
 
 function PlaceList(props) {
-	return (
-		<tbody>
-			{props.places.map((place, index) => (
-				<PlaceRow
-					key={`table-${JSON.stringify(place)}-${index}`}
-					place={place}
-					placeActions={props.placeActions}
-					selectedIndex={props.selectedIndex}
-					index={index}
-					distanceSettings = {props.distanceSettings}
-				/>
-			))}
-		</tbody>
-	);
+    return (
+        <tbody>
+            {props.places.map((place, index) => (
+                <PlaceRow
+                    key={`table-${JSON.stringify(place)}-${index}`}
+                    place={place}
+                    placeActions={props.placeActions}
+                    selectedIndex={props.selectedIndex}
+                    index={index}
+                    distanceSettings = {props.distanceSettings}
+                />
+            ))}
+        </tbody>
+    );
 }
 
 function PlaceRow(props) {
-	const [showFullName, toggleShowFullName] = useToggle(false);
-	const name = props.place.defaultDisplayName;
-	const location = latLngToText(placeToLatLng(props.place));
-	let distances = [0]; 
-	if (props.distanceSettings.serverDistance){ 
-		
-	 	distances = props.distanceSettings.serverDistance.distances;
-		
-	}
+    const [showFullName, toggleShowFullName] = useToggle(false);
+    const name = props.place.defaultDisplayName;
+    const location = latLngToText(placeToLatLng(props.place));
+    let distances = [0]; 
+    if (props.distanceSettings.distances){ 
+        
+        distances = props.distanceSettings.distances.leg;
+        
+    }
 
-	return (
-		<tr className={props.selectedIndex === props.index ? 'selected-row' : ''}>
-			<td
-				data-testid={`place-row-${props.index}`}
-				onClick={() =>
-					placeRowClicked(
-						toggleShowFullName,
-						props.placeActions.selectIndex,
-						props.index
-					)
-				}
-			>
-				<strong>{name}</strong>
-				<AdditionalPlaceInfo showFullName={showFullName} location={location} placeActions={props.placeActions} index={props.index} place={props.place}/>
-			</td>
-			<td align='right'> {distances[props.index]} </td>
-			<CumulativeLegDistance distances = {distances} index = {props.index}/>
-			<RowArrow toggleShowFullName={toggleShowFullName} index={props.index}/>
-		</tr>
-	);
+    return (
+        <tr className={props.selectedIndex === props.index ? 'selected-row' : ''}>
+            <td
+                data-testid={`place-row-${props.index}`}
+                onClick={() =>
+                    placeRowClicked(
+                        toggleShowFullName,
+                        props.placeActions.selectIndex,
+                        props.index
+                    )
+                }
+            >
+                <strong>{name}</strong>
+                <AdditionalPlaceInfo showFullName={showFullName} location={location} placeActions={props.placeActions} index={props.index} place={props.place}/>
+            </td>
+            <td align='right'> {distances[props.index]} </td>
+            <CumulativeLegDistance distances = {distances} index = {props.index}/>
+            <RowArrow toggleShowFullName={toggleShowFullName} index={props.index}/>
+        </tr>
+    );
 }
 
 function CumulativeLegDistance(props){
-	let total = 0 
- 	for (var i = 0; i < props.index; i++){ 
-		total+= props.distances[i];
-	}
-	return <td data-testid={`place-col-${props.index}`} align='right'> {total}</td>
-
+    let cumulativeArray = calcCumulative(props.distances);
+    return <td data-testid={`place-col-${props.index}`} align='right'> {cumulativeArray[props.index]}</td>
 }
 
 function AdditionalPlaceInfo(props) {
-	return (
-		<Collapse isOpen={props.showFullName}>
-			{props.place.formatPlace().replace(`${props.place.defaultDisplayName}, `, '')}
-			<br />
-			{props.location}
-			<br />
-			<PlaceActions placeActions={props.placeActions} index={props.index} />
-		</Collapse>
-	);
+    return (
+        <Collapse isOpen={props.showFullName}>
+            {props.place.formatPlace().replace(`${props.place.defaultDisplayName}, `, '')}
+            <br />
+            {props.location}
+            <br />
+            <PlaceActions placeActions={props.placeActions} index={props.index} />
+        </Collapse>
+    );
 }
 
 function placeRowClicked(toggleShowFullName, selectIndex, placeIndex) {
-	toggleShowFullName();
-	selectIndex(placeIndex);
+    toggleShowFullName();
+    selectIndex(placeIndex);
 }
 
 function RowArrow(props) {
-	return (
-		<td>
-			<BsChevronDown data-testid={`place-row-toggle-${props.index}`} onClick={props.toggleShowFullName}/>
-		</td>
-	);
+    return (
+        <td>
+            <BsChevronDown data-testid={`place-row-toggle-${props.index}`} onClick={props.toggleShowFullName}/>
+        </td>
+    );
 }
-function useDistances(places) {
-	const [serverUrl, setServerUrl] = useState(getOriginalServerUrl());
-	const [serverDistance, setServerDistance] = useState({distances: []});
 
-	useEffect(() => {
-		sendDistanceRequest();
-	}, [places]);
-
-	function processServerDistanceSuccess(distance, url) {
-		LOG.info('Switching to Server:', url);
-		setServerDistance(distance);
-		setServerUrl(url);
-	}
-
-	async function sendDistanceRequest() {
-		const distanceResponse = await sendAPIRequest({ 
-			requestType: 'distances', 
-			places : places,
-			earthRadius : 6571} , serverUrl);
-		if (distanceResponse) {
-			processServerDistanceSuccess(distanceResponse, serverUrl);
-		} else {
-			setServerDistance({distances: []});
-			//showMessage(`Distance request to ${serverUrl} failed. Check the log for more details.`, 'error');
-		}
-	}
-
-	return [{ serverUrl: serverUrl, serverDistance: serverDistance }, processServerDistanceSuccess,];
+export function useDistances(places, earthRadius, serverURL) {
+    const [leg, setLeg] = useState([]);
+    const [cumulative, setCumulative] = useState([]);
+    const [total, setTotal] = useState(0);
+    
+    const distances = {
+      leg: leg,
+      cumulative: cumulative,
+      total: total
+    }
+    
+    const distanceActions = {
+      setLeg: setLeg,
+      setCumulative: setCumulative,
+      setTotal: setTotal
+    }
+    
+    useEffect(() => {makeDistancesRequest(places, earthRadius, serverURL, distanceActions);},
+              [places,earthRadius])
+  
+    return {distances};
 }
+
+async function makeDistancesRequest(places, earthRadius, serverURL, distanceActions) {
+  
+    const {setLeg, setCumulative, setTotal} = distanceActions;
+  
+    const requestBody = { requestType: "distances", places: places, earthRadius: earthRadius};
+    const distancesResponse = await sendAPIRequest(requestBody, serverURL);
+
+    if (distancesResponse) {
+        setLeg(distancesResponse.distances);
+        setCumulative(calcCumulative(distancesResponse.distances))
+        setTotal(calcTotal(distancesResponse.distances))
+    }
+    else {
+      LOG.error(`Distance request to ${serverURL} failed. Check the log for more details.`, "error");
+    }
+}
+
+function calcCumulative(distances){
+    let cummArray = [];
+    let total = 0;
+    cummArray[0] = 0;
+    for (var i = 1; i < distances.length; i++){
+       total += distances[i-1];
+       cummArray[i] = total;
+    }
+  return cummArray;
+}
+
+function calcTotal(distances){
+  let total = 0;
+  for (var i = 0; i < distances.length; i++){
+     total+= distances[i];
+    }
+  return total;
+}
+
+
 
