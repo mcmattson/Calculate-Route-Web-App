2d0c2baee6dc35f32e1b4797e6a43d9f5a534438
maricodes <maria.lake.cs@gmail.com>
2022-11-29 12:41:40 -0700

    adding buildResponse and extending class

--- server/src/main/java/com/tco/requests/TourRequest.java
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -1,10 +1,15 @@
 package com.tco.requests;
 
-public class TourRequest{
+public class TourRequest extends Request{
     
     private String requestType;
     private Double earthRadius;
     private Double response;
     private Places places;
-    
+
+    @Override
+    public void buildResponse(){
+        
+    }
+
 }
