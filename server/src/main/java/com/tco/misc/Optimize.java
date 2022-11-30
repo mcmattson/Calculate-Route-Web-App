49ff8802291837d0ca0c24568899fc0396ac82cf
maricodes <maria.lake.cs@gmail.com>
2022-11-29 18:13:24 -0700

    implimentation of findBestTour() method

--- server/src/main/java/com/tco/misc/Optimize.java
+++ server/src/main/java/com/tco/misc/Optimize.java
@@ -2,14 +2,34 @@ package com.tco.requests;
 
 
 public class Optimize {
-    Places places; 
+    Places places;
+    long totalDistance; 
 
     public Optimize(Places places){
         this.places = places;
+        DistancesRequest distances = new DistancesRequest(places);
+        this.totalDistance = distances.buildDistanceList().total();
     }
 
     public Places findBestTour(){
-        return null;
+        Places bestTour = places;
+        long bestDistance = totalDistance;
+
+
+        for (Place start : places){
+            Places current = createNearestNeighborTour(start);
+            DistancesRequest distances = new DistancesRequest(current);
+            Distances currentDistanceList = distances.buildDistanceList();
+            long totalDistance = currentDistanceList.total();
+
+            if (totalDistance < bestDistance){
+                bestTour = current;
+                bestDistance = totalDistance;
+            }
+
+        }
+
+        return bestTour;
     }
 
     public Places createNearestNeighborTour(Place start){
