eb00fc29ea26cd9f1382732da483060eb84267be
maricodes <maria.lake.cs@gmail.com>
2022-11-29 18:54:51 -0700

    impliment findClosest

--- server/src/main/java/com/tco/misc/Optimize.java
+++ server/src/main/java/com/tco/misc/Optimize.java
@@ -1,4 +1,5 @@
 package com.tco.requests;
+import com.tco.misc.DistanceCalculator;
 
 
 public class Optimize {
@@ -47,18 +48,18 @@ public class Optimize {
     }
 
     public Place findClosest(Places unvisited, Place next){
-        Place closestPlace = unvisited(0);
+        Place closestPlace = unvisited.get(0);
         long closestDistance;
         DistanceCalculator calculator = new DistanceCalculator();
 
-        long distance = calculator.calculate(next, unvisited(0), earthRadius);
+        long distance = calculator.calculate(next, unvisited.get(0), earthRadius);
         closestDistance = distance;
 
-        for (int i = 1; i < unvisited.length(); i++){
-            distance = calculator.calculate(next, unvisited(i), earthRadius);
+        for (int i = 1; i < unvisited.size(); i++){
+            distance = calculator.calculate(next, unvisited.get(i), earthRadius);
             if (distance < closestDistance){
                 closestDistance = distance;
-                closestPlace = unvisited(i);
+                closestPlace = unvisited.get(i);
             }
         }
         return closestPlace;
