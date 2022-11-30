afbdd621fbdfe143c581bda13c3abd90b2462cf1
maricodes <maria.lake.cs@gmail.com>
2022-11-30 13:26:27 -0700

    insert method

--- server/src/main/java/com/tco/misc/Optimize.java
+++ server/src/main/java/com/tco/misc/Optimize.java
@@ -5,4 +5,23 @@ public class Optimize {
     Places places;
     long totalDistance; 
     Double earthRadius;
+
+    public Place findClosest(Places unvisited, Place next){
+        Place closestPlace = unvisited.get(0);
+        long closestDistance;
+        DistanceCalculator calculator = new DistanceCalculator();
+
+        long distance = calculator.calculate(next, unvisited.get(0), earthRadius);
+        closestDistance = distance;
+
+        for (int i = 1; i < unvisited.size(); i++){
+            distance = calculator.calculate(next, unvisited.get(i), earthRadius);
+            if (distance < closestDistance){
+                closestDistance = distance;
+                closestPlace = unvisited.get(i);
+            }
+        }
+        return closestPlace;
+    }
+    
 }
