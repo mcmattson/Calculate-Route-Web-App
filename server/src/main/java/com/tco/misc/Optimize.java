4b3e08d9ca936c07ac2c441be6ed13988506ab72
maricodes <maria.lake.cs@gmail.com>
2022-11-29 17:52:05 -0700

    bare bones Optimize and constructor

--- server/src/main/java/com/tco/misc/Optimize.java
+++ server/src/main/java/com/tco/misc/Optimize.java
@@ -1,3 +1,19 @@
+package com.tco.requests;
+
+
 public class Optimize {
-    
+    Places places; 
+
+    public Optimize(Places places){
+        this.places = places;
+    }
+
+    public Places findBestTour(){
+        return null;
+    }
+
+    public Places createNearestNeighborTour(Place start){
+        return null;
+    }
+
 }
