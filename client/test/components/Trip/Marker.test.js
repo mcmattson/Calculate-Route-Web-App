b5db9526abee012c4a6a12a51562444d886e066e
CathWar <cathryn2731@yahoo.com>
2022-11-30 12:21:40 -0700

    added skeleton

---
+++ client/test/components/Trip/Marker.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import user from '@testing-library/user-event';
+import { beforeEach, describe, expect, test, jest } from '@jest/globals';
+import { MOCK_PLACES } from '../../sharedMocks';
+import Marker from '../../../src/components/Trip/Map/Marker.js';
+
+describe('Marker', () => {
+
+    
+
+});
