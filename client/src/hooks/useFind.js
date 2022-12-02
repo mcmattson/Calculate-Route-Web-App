675ba6333738f2a85f6eb7f774e435f3d77eaf66
Matt <matthewcmattson@gmail.com>
2022-12-01 19:54:28 -0700

    Fix Line Smell

--- client/src/hooks/useFind.js
+++ client/src/hooks/useFind.js
@@ -4,39 +4,25 @@ import { getOriginalServerUrl, sendAPIRequest } from '../utils/restfulAPI';
 import { placesList } from '../components/Header/AddPlace';
 
 function useFind(match, limit, serverURL) {
-    limit = limitUndefinedNull(match, limit);
-    match = matchUndefinedNull(match);
-    let found, places = []
-    const type = ['airport'], where = ['US'], [serverUrl, setServerUrl] = useState(getOriginalServerUrl()), [serverFind, setServerFind] = useState({ places: [] });
+    limit = limitUndefinedNull(match, limit); match = matchUndefinedNull(match);
+    let found, places = []; const type = ['airport'], where = ['US'], [serverUrl, setServerUrl] = useState(getOriginalServerUrl()), [serverFind, setServerFind] = useState({ places: [] });
     let find = { serverFind }, findActions = { setServerFind: setServerFind };
-    useEffect(() => {
-        sendFindRequest(match, limit, serverURL, findActions);
-    }, [match, limit])
-    return { find };
-    function processServerFindSuccess(places, url) {
-        LOG.info('Switching to Server:', url);
-        setServerFind(places);
-        setServerUrl(url);
-    }
+    useEffect(() => { sendFindRequest(match, limit, serverURL, findActions); }, [match, limit]); return { find };
+    function processServerFindSuccess(places, url) { LOG.info('Switching to Server:', url); setServerFind(places); setServerUrl(url); }
     async function sendFindRequest(match, limit, serverURL, findActions) {
-        const { setServerFind } = findActions, map1 = new Map();
-        let name, index, latitude, longitude, findResponse, mapPlaces, i, mapSetUnknown;
+        const { setServerFind } = findActions, map1 = new Map(); let name, index, latitude, longitude, findResponse, mapPlaces, i, mapSetUnknown;
         try {
             const requestBody = { requestType: "find", match: match, type: type, where: where, limit: limit }; findResponse = await sendAPIRequest(requestBody, serverURL);
             found = setNewFound(findResponse.found, limit); //Set Limit to 10 if more than 10 
             if (found > 0) {
                 processServerFindSuccess(findResponse, serverUrl);
                 for (i = 0; i < found; i++) {
-                    places = findResponse.places[i];
-                    mapPlaces = setMapInfo(name = places.name, latitude = places.latitude, longitude = places.longitude, map1); //Clears and Sets Map
-                    map1.set('index', index = i);
-                    placesList(mapPlaces, found);
-                    setServerFind({ places: [mapPlaces] });
+                    places = findResponse.places[i]; mapPlaces = setMapInfo(name = places.name, latitude = places.latitude, longitude = places.longitude, map1); //Clears and Sets Map
+                    map1.set('index', index = i); placesList(mapPlaces, found); setServerFind({ places: [mapPlaces] });
                 }
             } else {
                 mapSetUnknown = setMapInfoUnknown(map1); //Clears and Sets Map to Unknown
-                placesList(mapSetUnknown, found);
-                setServerFind({ places: [] });
+                placesList(mapSetUnknown, found); setServerFind({ places: [] });
             }
         } catch (error) { }
     }
