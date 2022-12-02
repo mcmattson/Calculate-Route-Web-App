868c567ba9206c6a04c89c8306eff1b023050bff
Mikyla Bowen <mikyla.bowen@colostate.edu>
2022-12-01 17:50:47 -0700

    Add a tour request hook code.

--- client/src/hooks/useTour.js
+++ client/src/hooks/useTour.js
@@ -1,11 +1,34 @@
 import { useState, useEffect } from 'react';
 import { LOG } from '../utils/constants';
-import { sendAPIRequest } from '../utils/restfulAPI';
+import {getOriginalServerUrl, sendAPIRequest } from '../utils/restfulAPI';
 
-export function useTour(earthRadius, places, response, serverURL) {
 
+export function useTour(optimize, earthRadius, places, response, serverURL) {
+    const [serverTour, setServerTour] = useState({places: []});
+    const [serverUrl, setServerUrl] = useState(getOriginalServerUrl());
+
+    useEffect( () => {
+        makeTourRequest(earthRadius, places, response, serverURL);
+    }, optimize);  
+
+    return [{serverUrl: serverUrl, serverTour: serverTour}, processServerTourSuccess];
+}
+
+function processServerTourSuccess(places, url){
+    LOG.info('Switching to Server: ', url);
+    setServerTour(places);
+    setServerUrl(url);
 }
 
 async function makeTourRequest(earthRadius, places, response, serverURL) {
-  
+    const tourResponse = await sendAPIRequest({
+        requestType: 'tour',
+        earthRadius: earthRadius,
+        places: places, 
+        response: response}, serverUrl);
+    if (tourResponse) {
+        processServerTourSuccess(tourResponse,serverUrl);
+    } else {
+        setServerTour({places: []});
+    }
 }
