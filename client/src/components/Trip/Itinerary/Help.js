f4d5b822aa8bf986a637544188df4f1c1556ea9b
Ian Duncan <ianduncan2149@gmail.com>
2022-12-08 17:09:28 -0700

    Add instructions

--- client/src/components/Trip/Itinerary/Help.js
+++ client/src/components/Trip/Itinerary/Help.js
@@ -26,6 +26,15 @@ function HelpBody(props){
 	return(
 		<ModalBody>
 			<div style={{fontSize: '18px'}}>
+				<h1><strong>Instructions</strong></h1>
+				<p>1. The button in the top right will display the dropdown menu</p>
+				<p>2. Click about in the dropdown to see the team about page</p>
+				<p>3. Add home will add a location to the map at CSU</p>
+				<p>4. Add place will allow you to search for places and add them to the trip</p>
+				<p>5. Load file allows you to load in a predetermined trip</p>
+				<p>6. The remove all button is available in the drop down and on th UI, which will clear your trip</p>
+				<p>7. Switch the server using server settings</p>
+
 			</div>
 		</ModalBody>
 	);
